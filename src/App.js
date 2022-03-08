b72e44008be05e51a94314dc852ceb9a6fbcfd22
Lowry02 <lorenzocusin@MacBook-Air-di-Lorenzo.local>
2022-03-08 10:31:58 +0100

    Starting project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
+import $ from "jquery"
 import './App.css';
 
+// url di base
+const api_url = "https://finanz-developing.herokuapp.com/"
+// token di accesso
+const accessToken = "<< METTI IL TUO TOKEN >>"
+
 function App() {
+
+  // funzione che gestisce la richiesta
+  function handleRequest() {
+    $.ajax({
+      type: "GET",
+      url: api_url + "arguments",
+      contentType: "application/json",
+      beforeSend: (request) => request.setRequestHeader('Authorization', "Bearer " + accessToken),
+      success: (data) => console.log(data),
+      error: (message) => console.log(message)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>GUARDA LA CONSOLE</h1>
+      <button onClick={handleRequest}>Fai richiesta</button>
     </div>
   );
 }
